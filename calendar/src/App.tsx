a4b36efa32115677ddb8be042e9e825698f6a831
te-ing <hi2177@naver.com>
2023-03-12 14:36:18 +0900

    init: calendar 개발환경 초기설정

---
+++ calendar/src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "style/test.module.scss";
+
+function App() {
+  return (
+    <div>
+      <div className={styles.testCamel}>test</div>
+    </div>
+  );
+}
+
+export default App;
